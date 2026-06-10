dce5866dad4851092d68a5d94c290711a54492b1
MathiasSorin <62813347+MathiasSorin@users.noreply.github.com>
2026-06-10 02:39:24 -0400

    First commit
    Functional prototype

---
+++ arcane-images.js
@@ -0,0 +1,8 @@
+// Add the filename here whenever you drop a new image into images/Arcane/
+const ARCANE_IMAGES = [
+    'Arcane.png',
+    'ArcaneEnergize.png',
+    'ArcaneStrike.png',
+    'ArcaneRage.png',
+    'ArcaneConsequence.png',
+];
